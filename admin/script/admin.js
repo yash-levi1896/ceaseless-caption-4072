7ac9b1fd612f1c0da099ac1453a527f35a9f417b
Binod Okheda <binodokheda3@gmail.com>
2023-02-24 13:01:48 +0530

    branch updated

--- admin/script/admin.js
+++ admin/script/admin.js
@@ -1,6 +1,7 @@
 let container=document.querySelector(".container")
 let btncontainer=document.querySelector(".buttonwarpper");
 let total=document.getElementById("total-product")
+const popup = document.querySelector(".popup");
 console.log(container)
 
 window.addEventListener("load",()=>{
@@ -19,9 +20,21 @@ function fetchAndRender(){
      display(data,totalProduct)
  }
 }
+let cardId;
+/* <form>
+          <input type="text" placeholder="id" id="card-id">
+          <input type="text" placeholder="title" id="card-title">
+          <input type="text" placeholder="category" id="card-category">
+          <input type="number" placeholder="price" id="card-price">
+          <input type="number" placeholder="rating" id="card-rating">
+          <input type="submit" value="Submit">
+ </form> */
+  let cardid=document.getElementById("card-id")
+  let cardTitle=document.getElementById("card-title")
+  let cardCategory=document.getElementById("card-category")
+  let cardPrice=document.getElementById("card-price")
+  let cardRating=document.getElementById("card-rating")
 
-
-  
 function display(info,totalcount){
     btncontainer.innerHTML="";
     let buttonArray=[];
@@ -55,8 +68,33 @@ function display(info,totalcount){
         }
     })
    }
+   let editBtn=document.getElementsByClassName("card_edit_btn")
+   console.log(editBtn)
+   for(let btn of editBtn){
+       btn.addEventListener("click",()=>{
+        cardId=btn.dataset.id
+        fetch(`https://mock-apai.onrender.com/toys/${cardId}`)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            cardid.value=data.id;
+            cardTitle.value=data.title;
+            cardCategory.value=data.category;
+            cardPrice.value=data.price;
+            cardRating.value=data.Rating;
+            popup.style.display = "block";
+        })
+       })
+   }
 }
-
+let title=document.getElementById("card-title")
+popup.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    // Submit form data here using fetch or XMLHttpRequest
+    // After submission, hide the popup
+    popup.style.display = "none";
+  });
 function getcards(rating,category,image,price,title,id){
     let card=`
       <div class="card">
@@ -74,3 +112,6 @@ function getcards(rating,category,image,price,title,id){
     `
     return card
 }
+
+
+
