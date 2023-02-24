beb874bf8f99d70c9ca53402b1a2fe45c29d6618
yash-levi1896 <yashkumar18gupta@gmail.com>
2023-02-24 23:01:22 +0530

    product page

--- toys.js
+++ toys.js
@@ -1,14 +1,50 @@
 let container=document.getElementById("container");
-const baseurl=`https://mock-apai.onrender.com`;
+let button=document.getElementById("button-wrapper");
+let filt=document.getElementById("filter");
+const url=`https://mock-apai.onrender.com/toys`;
+let id;
+let btns=document.getElementById("sort");
+let btnp=document.getElementById("sortp");
+let employeedata=[];
 
-fetch(`${baseurl}/toys`)
-.then((res)=>{
-    return res.json();
-})
-.then((data)=>{
-    console.log(data);
-    display(data)
-});
+filt.addEventListener("change",()=>{
+    if(filt.value==""){
+    fetchandrendercard(`?_page=${1}&_limit=12`)
+    }else{
+       fetchandrendercard(`?category=${filt.value}&_limit=12&_page=${1}`)
+    }
+});   
+btns.addEventListener("click",()=>{
+    employeedata.sort(function(a,b){return b.Rating-a.Rating});
+
+    display(employeedata)
+})    
+btnp.addEventListener("click",()=>{
+    employeedata.sort(function(a,b){return a.price-b.price});
+
+    display(employeedata)
+})    
+
+fetchandrendercard(`?_page=${1}&_limit=12`)
+function fetchandrendercard(queryParamstring=null){
+    fetch(`${url}${queryParamstring ? queryParamstring:""}`,{
+       method:"GET",
+    
+    })
+    .then((res)=> {
+        let totalCount=res.headers.get("X-Total-Count")
+        console.log("total",totalCount);
+        showpagination(totalCount,12)
+        
+        return res.json();
+    })
+    .then((data)=>{
+        console.log(data)
+        employeedata=data;
+       // showpagination(data.totalPages)
+        display(employeedata);
+    })
+}
 
 
 function display(data){
@@ -18,9 +54,6 @@ function display(data){
         let div2=document.createElement("div");
         let im=document.createElement("img");
         im.src=element.image;
-        im.style.height="200px"
-        im.style.width="200px"
-        card.style.border="1px solid red"
 
         let title=document.createElement("h2");
         title.innerText=element.title;
@@ -36,16 +69,50 @@ function display(data){
     });
 }
 
+function showpagination(totalitems,x){
+    const totalpages=Math.ceil(totalitems/x);
+    console.log(totalpages)
+    function renderbutton(){
+        let arr=[];
+        for(let i=0;i<totalpages;i++){
+             arr.push(getbuttons(i+1,i+1));
+             
+        }
+       // console.log(arr)
+        return arr.join("")
+    }
+    button.innerHTML=`
+       ${ renderbutton()}`
 
+    let buttonpage=document.querySelectorAll(".button-data");
+    
+    for(let x of buttonpage){
+        x.addEventListener("click",(e)=>{
+            let dataid=e.target.dataset.id;
+            id=dataid;
+            if(filt.value==""){
+            fetchandrendercard(`?_page=${id}&_limit=12`)
+            }
+            else if(filt.value=="art & craft"){
+                fetchandrendercard(`?category=${filt.value}&_limit=12&_page=${id}`)
+            }
+            else if(filt.value=="Puzzle games & books"){
+                fetchandrendercard(`?category=${filt.value}&_limit=12&_page=${id}`)
+            }
+            else if(filt.value=="action-figure"){
+                fetchandrendercard(`?category=${filt.value}&_limit=12&_page=${id}`)
+            }
+            else if(filt.value=="Dolls"){
+                fetchandrendercard(`?category=${filt.value}&_limit=12&_page=${id}`)
+            }
+            else if(filt.value=="Vehicle & Train"){
+                fetchandrendercard(`?category=${filt.value}&_limit=12&_page=${id}`)
+            }
+            
+ })
 
-
-
-
-
-
-
-
-
+}
+}
 
 function getbuttons(id,text){
     return ` <button data-id=${id} class="button-data">${text}</button>`
