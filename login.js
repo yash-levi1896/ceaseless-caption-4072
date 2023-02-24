d3f0093d9c268d0f6d12122e7e6731172fee7353
VishalKNigam <vishalnigam1407@gmail.com>
2023-02-24 22:36:55 +0530

    added login features

--- login.js
+++ login.js
@@ -53,3 +53,11 @@
       }
       return false
   }
+  function myFunction() {
+    var x = document.querySelector(".myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
