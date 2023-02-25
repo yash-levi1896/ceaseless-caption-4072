3e536db76ad33f83adc3fcb1bf2dda2e4d4eb624
VishalKNigam <vishalnigam1407@gmail.com>
2023-02-25 19:29:40 +0530

    login functionalities

--- login.js
+++ login.js
@@ -1,63 +1,99 @@
 
-    let signUp = document.querySelector('.nav>button:nth-child(1)');
-    let logIn = document.querySelector('.nav>button:nth-child(2)');
+    let createAccount = document.querySelector('#createAccount');
+    // let logIn = document.querySelector('.nav>button:nth-child(2)');
     let logInForm = document.querySelector('#login')
     let signUpForm = document.querySelector('#signup')
-    
-    signUp.addEventListener("click",function(){
+    let adminbutton = document.querySelector("#adminbutton")
+    let adminForm = document.querySelector("#admin")
+    let signupbutton = document.querySelector("#signupbutton")
+    let Email = document.querySelector("#email")
+    let Password = document.querySelector("#password")
+    //let signinbutton = document.querySelector("#signinbutton")
+    createAccount.addEventListener("click",function(){
        
         logInForm.style.display = 'none';
         signUpForm.style.display = 'block';
+        
     })
-    logIn.addEventListener("click",function(){
-        logInForm.style.display = 'block';
-        signUpForm.style.display = 'none';    
+    signupbutton.addEventListener("click",function(){
+       
+      logInForm.style.display = 'block';
+      signUpForm.style.display = 'none';
+      
+  })
+  logInForm.addEventListener("submit",function(e){
+      e.preventDefault();
+      fetch(`https://mock-apai.onrender.com/register`)
+      .then(res=>res.json())
+      .then(data=>{
+        console.log(data)
+        let email = Email.value;
+        let password = password.value;
+        let flag = false;
+        for(let i=0; i<data.length; i++){
+          if(email==data[i].email && password == data[i].password){
+               flag = true;
+          }
+        }
+          if(flag === true){
+            alert("Sign In Successful!!")
+          }
+      })
+
+  })
+    // logIn.addEventListener("click",function(){
+    //     logInForm.style.display = 'block';
+    //     signUpForm.style.display = 'none';    
+    // })
+    adminbutton.addEventListener("click",function(){
+      logInForm.style.display = 'none';
+      adminForm.style.display = 'block'; 
     })
     
-     signUpForm.addEventListener("submit",function(ele){
-          ele.preventDefault();
-          let element = {
-            name2: signUpForm.name2.value,
-            email2: signUpForm.email2.value,
-            password2: signUpForm.password2.value,
-            //name: signUpForm.name2.value,
-            //name: signUpForm.name2.value,
+    //  signUpForm.addEventListener("submit",function(ele){
+    //       ele.preventDefault();
+    //       let element = {
+    //         name2: signUpForm.name2.value,
+    //         email2: signUpForm.email2.value,
+    //         password2: signUpForm.password2.value,
+    //         //name: signUpForm.name2.value,
+    //         //name: signUpForm.name2.value,
             
-          }
-          loginarray.push(element);
+    //       }
+    //       loginarray.push(element);
            
-           alert("User Registered Successfully!!");
-           logInForm.style.display = 'block';
-        signUpForm.style.display = 'none'; 
+    //        alert("User Registered Successfully!!");
+    //        logInForm.style.display = 'block';
+    //     signUpForm.style.display = 'none'; 
            
-     })
-     let email = logInForm.email.value;
-     let password = logInForm.password.value; 
-     logInForm.addEventListener("submit",function(el){
-        el.preventDefault();
-        if(registered(loginarray)){
-            alert("Login Successfully!!")
-            location.replace("./index.html");
+    //  })
+    //  let email = logInForm.email.value;
+    //  let password = logInForm.password.value; 
+    //  logInForm.addEventListener("submit",function(el){
+    //     el.preventDefault();
+        // if(registered(loginarray)){
+        //     alert("Login Successfully!!")
+        //     location.replace("./index.html");
 
-        }else {
-            alert("Please Register yourself!!")
-        }
+        // }else {
+        //     alert("Please Register yourself!!")
+        // }
         
-  })
-  function registered(data){
-      for(let i=0; i<data.length; i++){
-        if((logInForm.email.value == data[i].email2) && (logInForm.password.value ==  data[i].password2)){
-            return true;
-        }
+  // })
+  // function registered(data){
+  //     for(let i=0; i<data.length; i++){
+  //       if((logInForm.email.value == data[i].email2) && (logInForm.password.value ==  data[i].password2)){
+  //           return true;
+  //       }
         
-      }
-      return false
-  }
-  function myFunction() {
-    var x = document.querySelector(".myInput");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
+  //     }
+  //     return false
+  // }
+  // function myFunction() {
+  //   var x = document.querySelector(".myInput");
+  //   if (x.type === "password") {
+  //     x.type = "text";
+  //   } else {
+  //     x.type = "password";
+  //   }
+  // }
